1f8be27923a8046b9b806077da27b8f8fa886771
Marcin020 <marcinduda729@gmail.com>
2023-10-23 14:38:11 +0200

    Add project files.

---
+++ SportApp/SportApp/HomeFlyout.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SportApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomeFlyout : ContentPage
+    {
+        public ListView ListView;
+
+        public HomeFlyout()
+        {
+            InitializeComponent();
+
+            BindingContext = new HomeFlyoutViewModel();
+            ListView = MenuItemsListView;
+        }
+
+        private class HomeFlyoutViewModel : INotifyPropertyChanged
+        {
+            public ObservableCollection<HomeFlyoutMenuItem> MenuItems { get; set; }
+
+            public HomeFlyoutViewModel()
+            {
+                MenuItems = new ObservableCollection<HomeFlyoutMenuItem>(new[]
+                {
+                    new HomeFlyoutMenuItem { Id = 0, Title = "Home", TargetType=typeof(Home)},
+                    new HomeFlyoutMenuItem { Id = 1, Title = "Sport", TargetType=typeof(Home)},
+                    new HomeFlyoutMenuItem { Id = 2, Title = "Plan", TargetType=typeof(Home)},
+                    new HomeFlyoutMenuItem { Id = 3, Title = "Stoper", TargetType = typeof(Home)},
+                    new HomeFlyoutMenuItem { Id = 4, Title = "Profil", TargetType = typeof(Home)}
+                });
+            }
+
+            #region INotifyPropertyChanged Implementation
+            public event PropertyChangedEventHandler PropertyChanged;
+            void OnPropertyChanged([CallerMemberName] string propertyName = "")
+            {
+                if (PropertyChanged == null)
+                    return;
+
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+            #endregion
+        }
+    }
+}
