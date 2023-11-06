8ca281416bbc58f3569c807b24a9631612442690
Marcin020 <marcinduda729@gmail.com>
2023-11-06 22:24:50 +0100

    Dodanie do rejestracji pozycji wzrost, waga, wiek

--- SportApp/SportApp/App.xaml.cs
+++ SportApp/SportApp/App.xaml.cs
@@ -10,7 +10,7 @@ namespace SportApp
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage( new Menu());
+            MainPage = new NavigationPage( new Start());
         }
 
         protected override void OnStart()
